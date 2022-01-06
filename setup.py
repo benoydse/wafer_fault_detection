4b4af6e4e51be51b67262786549422ba56864d8a
Benoy99 <benoy@segmenty.com>
2022-01-06 23:55:42 +0530

    Update wafer fault detection

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="docker-heroku",
+    name="Wafer-Fault-Detection",
     version="0.0.3",
     description="ML project",
-    author="Avnish yadav", 
+    author="Benoy Eremita",
     packages=find_packages(),
     license="MIT"
 )
